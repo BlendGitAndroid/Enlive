acb00f91eb88744775ddebcff9226ae912af8057
BlendAndroid <1059152860@qq.com>
2021-09-25 15:18:11 +0800

    add version module(自定义plugin+includeBuild构建项目实战).

---
+++ version/src/main/java/com/blend/version/DependencyVersionPlugin.kt
@@ -0,0 +1,10 @@
+package com.blend.version
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class DependencyVersionPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+
+    }
+}
